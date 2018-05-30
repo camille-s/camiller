750ba9e7c192f91df904f95b7b549ae6be6a8674
camille-s <camille.seaberry@gmail.com>
2018-05-30 09:59:33 -0400

    Improve make_wide with vars_select

--- R/make_wide.R
+++ R/make_wide.R
@@ -10,14 +10,14 @@
 make_wide <- function(df, ..., group = variable) {
   gather_cols <- rlang::quos(...)
   grp_var <- rlang::enquo(group)
-  gather_names <- purrr::map_chr(rlang::exprs(...), as.character)
+  gather_names <- tidyselect::vars_select(names(df), !!!gather_cols)
 
   df %>%
     tidyr::gather(key = type, value = value, !!!gather_cols) %>%
     dplyr::mutate(!!rlang::quo_name(grp_var) := !!grp_var %>% forcats::fct_inorder() %>% forcats::fct_rev()) %>%
     dplyr::mutate(type = as.factor(type) %>% forcats::fct_relevel(gather_names)) %>%
     tidyr::unite("grp", !!grp_var, type, sep = "_", remove = F) %>%
-    # dplyr::mutate(grp = as.factor(grp) %>% forcats::fct_reorder2(!!grp_var, type, dplyr::first)) %>%
+    dplyr::mutate(grp = as.factor(grp) %>% forcats::fct_reorder2(!!grp_var, type, dplyr::first)) %>%
     dplyr::select(-!!grp_var, -type) %>%
     dplyr::filter(!is.na(value)) %>%
     tidyr::spread(key = grp, value = value)
