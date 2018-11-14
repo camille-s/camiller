4448e549eff755fd1715a4738e3fc0060213499a
camille-s <camille.seaberry@gmail.com>
2018-11-13 21:00:09 -0500

    Update test to reflect lowercases in cap_first

--- tests/testthat/test-cap_first.R
+++ tests/testthat/test-cap_first.R
@@ -4,5 +4,5 @@ library(testthat)
 
 test_that("capitalizes correctly", {
   words <- c("camille", "new haven", "new Haven")
-  expect_equal(cap_first(words), c("Camille", "New haven", "New Haven"))
+  expect_equal(cap_first(words), c("Camille", "New haven", "New haven"))
 })
