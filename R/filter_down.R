06ba23433ef3074aba5d142dade90574b55d4828
camille-s <camille.seaberry@gmail.com>
2021-12-02 14:12:53 -0500

    Add streak handling to filter_after

--- R/filter_down.R
+++ R/filter_down.R
@@ -10,7 +10,10 @@
 #' @param ... A set of one or more logical conditions. This follows what is
 #' allowed for `dplyr::filter`, though without the comma shorthand to combine
 #' multiple conditions.
-#' @param .include_first For `filter_after`, logical: if `FALSE` (the default),
+#' @param .streak For `filter_after`, logical: if `TRUE` (the default),
+#' all consecutive occurrences of the logical conditions are considered a match,
+#' rather than just the first occurrence.
+#' @param .include_first (DEPRECATED) For `filter_after`, logical: if `FALSE` (the default),
 #' the row that flags the condition will be excluded, returning only the rows
 #' after the occurrence. If `TRUE`, the row that matches the condition is included.
 #' If this is multiple rows, only the last is included. Using this will likely
@@ -46,34 +49,43 @@
 #' filter_until(messy_summary, x1 == "Weights" & is.na(x2))
 #' filter_after(messy_summary, x1 == "Weights" & is.na(x2))
 #'
-#' # In some cases, it would be useful to hold onto those headings:
-#' filter_after(messy_summary, x1 == "Weights" & is.na(x2), .include_first = TRUE)
 #' @export
 #' @rdname filter_down
-#'
-#' @import rlang
-#' @import dplyr
 filter_until <- function(.data, ...) {
   q <- rlang::quos(...)
-  tryCatch(
-    error = function(e) stop("An error occurred--most likely, your conditions are split by commas.\nUnlike dplyr::filter, this function requires '&' or '|' in between conditions, not commas."),
-    dplyr::mutate(.data, dummy = cumsum(!!!q) == 0)
-  )
+  test_filter(.data, q)
   dplyr::filter(.data, cumsum(!!!q) == 0)
 }
 
 #' @export
 #' @rdname filter_down
-filter_after <- function(.data, ..., .include_first = FALSE) {
+filter_after <- function(.data, ..., .streak = TRUE) {
   q <- rlang::quos(...)
+  test_filter(.data, q)
+  matches <- .data %>%
+    dplyr::mutate(.match = purrr::pmap_lgl(list(!!!q), all),
+                  .occur = cumsum(.match) > 0)
+  if (.streak) {
+    out <- matches %>%
+      dplyr::filter(.occur) %>%
+      dplyr::filter(streak(.match) > 1)
+  } else {
+    out <- matches %>%
+      dplyr::filter(.occur & dplyr::lag(.occur))
+  }
+  out %>%
+    dplyr::select(-.match, -.occur)
+}
+
+streak <- function(x) {
+  rl <- rle(x)$lengths
+  rep(seq_along(rl), times = rl)
+}
+
+test_filter <- function(.data, q) {
+  # q <- rlang::quos(...)
   tryCatch(
     error = function(e) stop("An error occurred--most likely, your conditions are split by commas.\nUnlike dplyr::filter, this function requires '&' or '|' in between conditions, not commas."),
     dplyr::mutate(.data, dummy = cumsum(!!!q) == 0)
   )
-  out <- dplyr::filter(.data, cumsum(!!!q) > 0)
-  if (.include_first) {
-    out
-  } else {
-    out[-1,]
-  }
 }
--- tests/testthat/test-filter_down.R
+++ tests/testthat/test-filter_down.R
@@ -17,22 +17,6 @@ test_that("filter_down properly excludes rows", {
   expect_equal(nrow(a1), 2)
 })
 
-test_that("filter_down properly includes rows", {
-  messy_summary <- tibble::tribble(
-    ~x1,        ~x2,
-    "A",        1,
-    "B",        5,
-    "C",        9,
-    "Weights",  NA,
-    "A",        0.2,
-    "B",        0.5
-  )
-  a1 <- filter_after(messy_summary, x1 == "Weights")
-  a2 <- filter_after(messy_summary, x1 == "Weights", .include_first = TRUE)
-  expect_equal(nrow(a1), 2)
-  expect_equal(nrow(a2), 3)
-})
-
 test_that("filter_down handles multiple conditions", {
   messy_notes <- tibble::tribble(
     ~x1,       ~x2,        ~x3,
@@ -41,7 +25,9 @@ test_that("filter_down handles multiple conditions", {
     "Source:", "xyz.com",  NA,
     "Date:",   "Jan",      2021
   )
+  u1 <- filter_until(messy_notes, grepl("\\:", x1) & is.na(x3))
   a1 <- filter_after(messy_notes, grepl("\\:", x1) & is.na(x3))
+  expect_equal(nrow(u1), 2)
   expect_equal(nrow(a1), 1)
 })
 
@@ -55,5 +41,26 @@ test_that("filter_down throws error for commas", {
   )
   a1 <- filter_after(messy_notes, grepl("\\:", x1) & is.na(x3))
   expect_equal(nrow(a1), 1)
+  expect_error(filter_until(messy_notes, grepl("\\:", x1), is.na(x3)))
   expect_error(filter_after(messy_notes, grepl("\\:", x1), is.na(x3)))
 })
+
+test_that("filter_after handles streaks", {
+  messy_summary <- tibble::tribble(
+    ~x1, ~x2,
+    "A",   1,
+    "B",   5,
+    "C",   9,
+    "Weights",  NA,
+    "Weighted norm",   0,
+    "A", 0.2,
+    "B", 0.5,
+    "Weights",   1
+  )
+  a1 <- filter_after(messy_summary, grepl("Weight", x1), .streak = TRUE)
+  a2 <- filter_after(messy_summary, grepl("Weight", x1), .streak = FALSE)
+  expect_equal(nrow(a1), 3)
+  expect_equal(nrow(a2), 4)
+})
+
+
